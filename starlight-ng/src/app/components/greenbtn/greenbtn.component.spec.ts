ebc8e1e3c2da94e80cb3ece07893e92b6b3a0ba5
Stef Tapsoba <set89@drexel.edu>
2023-02-25 21:32:19 -0500

    Fix issue with Greenbtn Component.

--- starlight-ng/src/app/components/greenbtn/greenbtn.component.spec.ts
+++ starlight-ng/src/app/components/greenbtn/greenbtn.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import GreenbtnComponent from './greenbtn.component';
+import { GreenbtnComponent } from './greenbtn.component';
 
 describe('GreenbtnComponent', () => {
   let component: GreenbtnComponent;
